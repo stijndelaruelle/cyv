ae49059d321a368f16923cbdb19763cc8e0d9efd
Stijn Delaruelle <stijndelaruelle@gmail.com>
2015-05-06 22:46:21 +0200

    Removed some AI modes (moves took too long to calculate, will return later)

--- Assets/Scripts/UI/DifficultyChanger.cs
+++ Assets/Scripts/UI/DifficultyChanger.cs
@@ -15,18 +15,18 @@ public class DifficultyChanger : MonoBehaviour
 
     private void Awake()
     {
-        m_VisualDifficultyType = new string[9];
+        m_VisualDifficultyType = new string[6];
         m_VisualDifficultyType[0] = "Drunkard";  //Standard Easy
-        m_VisualDifficultyType[1] = "Stanger";   //Standard Medium
-        m_VisualDifficultyType[2] = "Wizard";    //Standard Hard
+        m_VisualDifficultyType[1] = "Stranger";  //Standard Medium
+        //m_VisualDifficultyType[2] = "Wizard";    //Standard Hard
 
-        m_VisualDifficultyType[3] = "Merchant";  //Aggressive Easy
-        m_VisualDifficultyType[4] = "Sellsword"; //Aggressive Medium
-        m_VisualDifficultyType[5] = "Knight";    //Aggressive Hard
+        //m_VisualDifficultyType[3] = "Merchant";  //Aggressive Easy
+        m_VisualDifficultyType[2] = "Sellsword"; //Aggressive Medium
+        m_VisualDifficultyType[3] = "Knight";    //Aggressive Hard
 
-        m_VisualDifficultyType[6] = "Beggar";    //Defensive Easy
-        m_VisualDifficultyType[7] = "Smuggler";  //Defensive Medium
-        m_VisualDifficultyType[8] = "Banker";    //Defensive Hard
+        //m_VisualDifficultyType[6] = "Beggar";    //Defensive Easy
+        m_VisualDifficultyType[4] = "Smuggler";  //Defensive Medium
+        m_VisualDifficultyType[5] = "Banker";    //Defensive Hard
     }
 
     private void Start()
@@ -60,11 +60,11 @@ public class DifficultyChanger : MonoBehaviour
     private void UpdateSettings()
     {
         //Determine the difficulty
-        int difficulty = (m_ID % 3);
+        int difficulty = (m_ID % 2);
         GameplayManager.Instance.NewGameSetup.m_AIDifficulty = difficulty;
 
         //Determine the PlayStyle
-        int type = (m_ID / 3);
+        int type = (m_ID / 2);
         GameplayManager.Instance.NewGameSetup.m_AIType = (AIType)type;
 
         //Set the text label
