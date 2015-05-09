b2f9c1249f8fd1a385a341bfda6884bac4d8d75c
Stijn Delaruelle <stijndelaruelle@gmail.com>
2015-05-09 16:36:20 +0200

    Added version number & icon.

---
+++ Assets/Scripts/UI/VersionNumber.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Reflection;
+
+public class VersionNumber : MonoBehaviour
+{
+    [SerializeField]
+    private Text m_TextField;
+
+	void Start ()
+    {
+	    //Get the version number & set it to the textfield
+        string version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+        m_TextField.text = "Version: " + version;
+	}
+	
+}
