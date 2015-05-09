09f35a09c1fc74bd8c22cc77f0d01a14a080196a
Stijn Delaruelle <stijndelaruelle@gmail.com>
2015-05-09 02:45:08 +0200

    Added simple twitter button.

---
+++ Assets/Scripts/UI/LinkButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LinkButton : MonoBehaviour
+{
+    [SerializeField]
+    private string m_Link;
+
+    public void GoToLink()
+    {
+        if (string.IsNullOrEmpty(m_Link))
+            return;
+
+        Application.OpenURL(m_Link);
+    }
+}
